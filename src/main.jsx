d6f9fd139d3787841d3cce3ae74c6da3e83c06cf
emonmahdi <developeremon1997@gmail.com>
2025-01-25 21:20:10 +0600

    folder structure [Done]

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./shared/Footer.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Login from "./pages/Login/Login.jsx";
+import AboutUs from "./pages/AboutUs/AboutUs.jsx";
+import Navbar from "./shared/Navbar.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  </StrictMode>
+);
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <p>Body Content</p>
+    </div>
+  );
+};
+
+export default Home;
